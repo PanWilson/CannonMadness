5f55ca6b61e217be34331636260c1bb6c7877041
PanWilson <PanWilsonn@wp.pl>
2018-07-08 14:45:52 +0200

    projectile

--- CannonMadness/Assets/ProjectileComponent.cs
+++ CannonMadness/Assets/ProjectileComponent.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ProjectileComponent : MonoBehaviour {
 
+    [SerializeField] ForceMode ForceMode = ForceMode.Impulse;
+
+    Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void LaunchProjectile(Vector3 direction, float force)
+    {
+        if (rb) {
+            rb.AddForce(direction * force,ForceMode);
+        }
+    }
 }
