33e979d92917b83ff8d22c24c7b76ac570f3162d
Jakub <jakub.kicmal@gmail.com>
2018-07-09 15:28:18 +0200

    trees, rocks, fixed materials

---
+++ CannonMadness/Assets/Main/Code/RotateThis.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RotateThis : MonoBehaviour {
+
+    public bool rotate;
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        if (rotate)
+        {
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+    }
+
+}
