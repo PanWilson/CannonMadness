f89a9d1ca2e05973aa89bb5139115ecff3b15047
Jakub <jakub.kicmal@gmail.com>
2018-07-10 15:16:33 +0200

    burza

---
+++ CannonMadness/Assets/Main/Camera/GodModeCameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GodModeCameraMovement : MonoBehaviour {
+
+    public float movementSpeed;
+
+    public KeyCode forward = KeyCode.W;
+    public KeyCode forward = KeyCode.W;
+    public KeyCode forward = KeyCode.W;
+    public KeyCode forward = KeyCode.W;
+
+    private void Update()
+    {
+            
+    }
+
+}
