7e97d731054f418a8961f0ecfb7829396b073600
Jakub <jakub.kicmal@gmail.com>
2018-07-14 13:44:17 +0200

    Air Pipes

---
+++ CannonMadness/Assets/Main/Events/AirPipe/AirPipe.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class AirPipe : MonoBehaviour {
+
+    public Transform airTarget;
+    public float power;
+    private Vector3 direction;
+    private bool playerInside;
+    private Rigidbody rb;
+
+    public void Awake()
+    {
+        direction = transform.position - airTarget.position;
+        direction = direction.normalized;
+    }
+
+    private void Update()
+    {
+        if (playerInside && rb != null)
+        {
+            rb.AddForce(direction * power);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (rb == null)
+                rb = other.transform.parent.GetComponent<Rigidbody>();
+            playerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+}
