24ec806bdb54fb12ec8340531e133ecaa15c0c22
Jakub <jakub.kicmal@gmail.com>
2018-07-14 16:15:43 +0200

    Bin and basketball events

---
+++ CannonMadness/Assets/Main/Events/Bin/Bin.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Bin : MonoBehaviour {
+
+    bool completed = false;
+    bool active = true;
+
+    private void Awake()
+    {
+        if (completed)
+        {
+            active = false;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (active)
+            {
+                Debug.Log("Bin Event successfuly completed");
+                OnEventCompletion();
+            }
+        }
+    }
+
+    public void OnEventCompletion()
+    {
+        active = false;
+        gameObject.SetActive(false);
+
+        // Other things like sound or shit like that
+    }
+}
