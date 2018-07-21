bf15e1ea36650b87b8fc453f30a82ba386296f15
Jakub <jakub.kicmal@gmail.com>
2018-07-21 17:53:55 +0200

    testing level

--- CannonMadness/Assets/Main/Code/RotateThis.cs
+++ CannonMadness/Assets/Main/Code/RotateThis.cs
@@ -3,13 +3,21 @@
 public class RotateThis : MonoBehaviour {
 
     public bool rotate;
-    public float rotationSpeed;
+
+    [Space]
+    public float speed;
+
+    [Header("Axis ammount")]
+    [Range(0f,1f)] public float x = 0f;
+    [Range(0f, 1f)] public float y = 0f;
+    [Range(0f, 1f)] public float z = 0f;
 
     private void Update()
     {
         if (rotate)
         {
-            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+            Vector3 rotation = new Vector3(x, y, z) * speed * Time.deltaTime;
+            transform.Rotate(rotation);
         }
     }
 
