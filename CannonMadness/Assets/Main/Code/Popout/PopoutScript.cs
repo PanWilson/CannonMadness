fd6c4b89dd70b555acaf3a7932dc32d5b999c686
Loxon1282 <loxon1282@gmail.com>
2018-08-06 01:08:43 +0200

    Popouty/scena/VisualAccessoriesManager

---
+++ CannonMadness/Assets/Main/Code/Popout/PopoutScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PopoutScript : MonoBehaviour {
+
+
+	[Range(0.2f,5.0f)]
+	public float time = 1.0f;
+	TextMeshProUGUI text;
+	Vector3 startPosition;
+	public GameObject objParent;
+	
+	void Start() {
+	text = GetComponent<TextMeshProUGUI>();
+	startPosition = transform.position;	
+	StartCoroutine(Destroy());
+	
+	}
+	void Update () {
+		if(objParent)
+			transform.position=objParent.transform.position;
+
+		transform.position = Vector3.Slerp(transform.position,startPosition+Vector3.up*5.0f,Time.deltaTime*2*time);
+
+		transform.LookAt(Camera.main.transform);
+		
+		text.color = new Color(text.color.r,text.color.g,text.color.b,Mathf.Lerp(1.0f,0,Time.time));
+	}
+
+	public IEnumerator Destroy(){
+		yield return new WaitForSecondsRealtime(time*3);
+		Destroy(transform.parent.gameObject);
+	}
+}
---
+++ CannonMadness/Assets/Main/Code/Popout/VisualAccessoriesManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualAccessoriesManager : MonoBehaviour {
+
+	private static VisualAccessoriesManager _instance;
+	public static VisualAccessoriesManager Instance { get { return _instance; } }
+
+
+	public GameObject TextPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CratePopOut(){
+		Instantiate(TextPrefab);
+	}
+	public void CratePopOut(GameObject elementToBuckle){
+		Instantiate(TextPrefab);
+		TextPrefab.GetComponentInChildren<PopoutScript>().objParent = elementToBuckle;
+	}
+}
