3eb6d45e7c14faf752820ddda86bb88495a112e5
Loxon1282 <loxon1282@gmail.com>
2018-08-09 07:22:25 +0200

    Visual accesorries manager

--- CannonMadness/Assets/Main/Code/Popout/VisualAccessoriesManager.cs
+++ CannonMadness/Assets/Main/Code/Popout/VisualAccessoriesManager.cs
@@ -9,12 +9,23 @@ public class VisualAccessoriesManager : MonoBehaviour {
 
 
 	public GameObject TextPrefab;
-	// Use this for initialization
+	//Camera shake
+
+	public bool debugMode = false;
+	public float shakeAmount;
+	public float shakeDuration;
+ 
+	float shakePercentage;
+	float startAmount;
+	float startDuration;
+ 
+	bool isRunning = false;	
+ 
+	public bool smooth;
+	public float smoothAmount = 5f;
 	void Start () {
-		
+		if(debugMode) ShakeCamera ();
 	}
-	
-	// Update is called once per frame
 	void Update () {
 		
 	}
@@ -25,5 +36,45 @@ public class VisualAccessoriesManager : MonoBehaviour {
 	public void CratePopOut(GameObject elementToBuckle){
 		Instantiate(TextPrefab);
 		TextPrefab.GetComponentInChildren<PopoutScript>().objParent = elementToBuckle;
+	}
+	void ShakeCamera() {
+ 
+		startAmount = shakeAmount;
+		startDuration = shakeDuration;
+ 
+		if (!isRunning) StartCoroutine (Shake());
+	}
+ 
+	public void ShakeCamera(float amount, float duration) {
+ 
+		shakeAmount += amount;
+		startAmount = shakeAmount;
+		shakeDuration += duration;
+		startDuration = shakeDuration;
+ 
+		if(!isRunning) StartCoroutine (Shake());
+		}
+
+ 
+	IEnumerator Shake() {
+		isRunning = true;
+ 
+		while (shakeDuration > 0.01f) {
+			Vector3 rotationAmount = Random.insideUnitSphere * shakeAmount;
+			rotationAmount.z = 0;
+ 
+			shakePercentage = shakeDuration / startDuration;
+ 
+			shakeAmount = startAmount * shakePercentage;
+			shakeDuration = Mathf.Lerp(shakeDuration, 0, Time.deltaTime);
+ 
+			if(smooth)
+				transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotationAmount), Time.deltaTime * smoothAmount);
+			else
+				transform.localRotation = Quaternion.Euler (rotationAmount);
+			yield return null;
+		}
+		transform.localRotation = Quaternion.identity;
+		isRunning = false;
 	}
 }
